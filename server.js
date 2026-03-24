f988ae244d3ba14ee6254b4cdc8de0533bbceb47
Sujan Deep <135427392+sujandeeep@users.noreply.github.com>
2026-03-24 17:14:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from my CI/CD app!' })
+  res.json({ message: 'Hello from my CI/CD app!!-- I'm Sujan' })
 })
 
 app.get('/health', (req, res) => {
