a9a744840e245242d166e7699058318ce6694507
Sujan Deep <135427392+sujandeeep@users.noreply.github.com>
2026-03-25 12:38:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/health', (req, res) => {
   res.status(200).json({ status: 'ok' })
 })
 
+app.get('/hello', (req, res) => {
+  res.json({ message: 'Hey Sujan!' })
+})
+
 app.listen(3000, () => {
   console.log('Server running on port 3000')
 })
